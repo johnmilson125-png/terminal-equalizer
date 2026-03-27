da78bb636acab0e16d55f8b13c104e9818a9036d
majockbim <majock.bim1@gmail.com>
2026-03-27 16:00:56 -0400

    test/Experiment with signal-processor

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,32 @@ int main() {
     RenderEqualizer o;
     SignalProcessor s;
     // o.Display();
+
+    // Get a single buffer, and loop through each index
+    for(UINT32 i = 0; i < 5; i++) {
+        /* NOTE:
+        * this 'Accumulate()' function may need to
+        * be called multiple times before 'GetFFTBuffer()'
+        * is called, since each packet has a size of 480
+        * and we are Accumulating a singular packet upon
+        * each call of the function
+        */
+        s.Accumulate();
+    }
+    auto buff = s.GetFFTBuffer();
+    std::cout << "Size of buffer: " <<  buff.size() << "\n";
+    for(auto i : buff) {
+        std::cout << i << "\n";
+    }
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+
     while(true) {
         // o.DisplayBuffer();
         s.Accumulate();
 
         if(s.isFull()) {
-            auto buff = s.GetFFTBuffer();
+            buff = s.GetFFTBuffer();
             std::cout << "Size of buffer: " << buff.size() << "\n";
         }
     }
