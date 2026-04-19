79f85069f7d1be72825e80eabcaf996bd65b55cf
majockbim <majock.bim1@gmail.com>
2026-04-19 16:35:24 -0400

    refactor/move signal processing test

--- src/main.cpp
+++ src/main.cpp
@@ -13,28 +13,6 @@ int main() {
     // o.Display();
 
     /* signal_processor test
-    // Get a single buffer, and loop through each index
-    for(UINT32 i = 0; i < 5; i++) {
-
-         NOTE:
-        * this 'Accumulate()' function may need to
-        * be called multiple times before 'GetFFTBuffer()'
-        * is called, since each packet has a size of 480
-        * and we are Accumulating a singular packet upon
-        * each call of the function
-        
-        s.Accumulate();
-    }
-    auto buff = s.GetFFTBuffer();
-    std::cout << "Size of buffer: " <<  buff.size() << "\n";
-    for(auto i : buff) {
-        std::cout << i << "\n";
-    }
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-    */
-
-    /* signal_processor test
     while(true) {
         // o.DisplayBuffer();
         s.Accumulate();
