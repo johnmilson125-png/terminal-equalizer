81f9f93fa6d9978b794c65501fde90dfeea2cc95
majockbim <majock.bim1@gmail.com>
2026-04-29 02:03:26 -0400

    fix/ASCII clean up + visualizer height limits + fixes

--- inc/audio/engine.hpp
+++ inc/audio/engine.hpp
@@ -27,13 +27,20 @@ public:
     float FloatVolume();
 
     // in AudioEngine header
-    UINT32 GetSampleRate() const { return pWAVEFORMATEX->nSamplesPerSec; }
+    UINT32 GetSampleRate() const { return pWAVEFORMATEX ? pWAVEFORMATEX->nSamplesPerSec : 44100; }
+    bool IsRunning() const { return running; }
+    void Stop() { running = false; }
 
 private:
     // Initialize object
     AudioEngine();
 
     ~AudioEngine();
+
+    bool running = true;
+    WORD bitsPerSample = 0;
+    WORD nChannels = 0;
+    bool isFloat = false;
 
     // Initialize member variables (volume level)
     IMMDeviceEnumerator* pEnumerator = NULL;
--- inc/main.h
+++ inc/main.h
@@ -6,6 +6,3 @@
 #include "../inc/ui/visualizer.hpp"
 #include "../inc/processing/signal_processor.hpp"
 #include "../inc/math/fft.hpp"
-
-extern std::mutex magMutex;
-extern std::vector<double> sharedMagnitudes;
